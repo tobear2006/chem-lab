828646024cbefc10e63d69f80b2fcfb762181923
tobear2006 <tobear2006@gmail.com>
2026-07-25 15:36:28 +0800

    v3.5：修复端口连接不灵敏

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker - 虚拟化学实验室 PWA v3.2 - 强制更新版
-const CACHE_VERSION = 'chem-lab-v3-20260725e';
-const APP_VERSION = '3.0.4';
+const CACHE_VERSION = 'chem-lab-v3-20260725f';
+const APP_VERSION = '3.0.5';
 const ASSETS = [
   './',
   './index.html',
